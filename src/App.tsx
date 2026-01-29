9e0a6b5e2f989ae57f575690b543b5c4ef0bec32
bala-murali-k <k.balamurali0701@gmail.com>
2026-01-29 07:27:55 +0530

    theme system is implemented

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import { Routes, Route } from 'react-router-dom'
 
 // Component Imports
 import MainLayout from './layout/main.layout.component.tsx'
+import ThemeContextComponent from './context/theme/theme.context.component.tsx'
 
 function App() {
 
   return (
 	<Routes>
-		<Route element={ <MainLayout /> }>
+		<Route element={
+			<ThemeContextComponent>
+				<MainLayout />
+			</ThemeContextComponent>
+			}
+		>
 			<Route path="" element={<>This is the home page</>} />
 			<Route path="*" element={ <>This is the error page for this.</> } />
 		</Route>
--- src/layout/main.layout.component.tsx
+++ src/layout/main.layout.component.tsx
@@ -1,28 +1,29 @@
 // Required imports
-import { createContext } from 'react'
-import { Box, Tooltip, Stack, IconButton, Typography, CssBaseline } from '@mui/material'
+import { useContext } from 'react'
+import { Box, Tooltip, Stack, IconButton, Typography } from '@mui/material'
 import { Outlet } from 'react-router-dom'
-
+import { ThemeContext } from './../context/theme/theme.context.component.tsx'
 // Required objects
-export const GlobalContext = createContext(null)
 
 
 export default function MainLayout ({  }) {
 	
-	const globalVariables = {
-		primaryTheme: 'light',
-	}
+	const { currentTheme, setCurrentTheme } = useContext(ThemeContext)
 	
 	return (
-		<GlobalContext.Provider value={globalVariables}>
-			<Box sx={{ width: '100vw', height: '100vh' }}>
-				<Box sx={{ width: '100%', height: '8%', boxShadow: 3, display: 'flex', alignItems: 'center' }}>
-					
-				</Box>
-				<Box>
-					<Outlet />
-				</Box>
+		<Box sx={{ width: '100vw', height: '100vh', bgcolor: 'tonal.a10', transition: 'background-color 0.4s ease-in-out' }}>
+			<Box sx={{ width: '100%', height: '8%', boxShadow: 3, display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+				<IconButton variant='small' 
+					onClick={() => {
+						setCurrentTheme(currentTheme === 'light' ? 'dark' : 'light')
+					}}
+				>
+					<span className="material-symbols-outlined" style={{ fontSize: '15px' }}>routine</span>
+				</IconButton>
 			</Box>
-		</GlobalContext.Provider>
+			<Box>
+				<Outlet />
+			</Box>
+		</Box>
 	)
 }
